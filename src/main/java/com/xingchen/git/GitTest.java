09017e7e6842376f7bcb189cd859fb47ecb6a608
xingchen <418124197@qq.com>
2021-09-25 10:32:02 +0800

    1

--- src/main/java/com/xingchen/git/GitTest.java
+++ src/main/java/com/xingchen/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     System.out.println("hot-fox merge test1");
     System.out.println("master commit");
     System.out.println("hot-fix commit");
+    System.out.println("push test");
   }
 }
